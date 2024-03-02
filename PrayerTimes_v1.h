c43f9ddab9da12f01259897b4b59ed8882716d62
chatGaPenTing <161785031+chatGaPenTing@users.noreply.github.com>
2024-03-02 10:33:05 +0700

    Update PrayerTimes_v1.h

--- PrayerTimes_v1.h
+++ PrayerTimes_v1.h
@@ -1,9 +1,6 @@
 #ifndef Morse_h
 #define Morse_h
 
-#include <Arduino.h>
-#include <string.h>
-
 static const int NUM_ITERATIONS = 1; // number of iterations needed to compute times
 
 /* ------------------------------------------------------------------------------------------------- */
