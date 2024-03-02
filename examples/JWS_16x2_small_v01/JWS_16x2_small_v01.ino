f4040cdc93305685e1d9a9793fe606a934b55a7f
chatGaPenTing <161785031+chatGaPenTing@users.noreply.github.com>
2024-03-02 08:41:29 +0700

    Update JWS_16x2_small_v01.ino
    Rearrange global declaration for GMT, latitude and longitude

--- examples/JWS_16x2_small_v01/JWS_16x2_small_v01.ino
+++ examples/JWS_16x2_small_v01/JWS_16x2_small_v01.ino
@@ -1,42 +1,50 @@
-#include <PrayerTimes.h>  // Librari untuk menghitung waktu sholat
+#include <PrayerTimes.h> // Librari untuk menghitung waktu sholat
 
-#include <RTClib.h>  // Librari Real-Time Clock (RTC)
-RTC_DS1307 rtc;  // Inisialisasi objek RTC
+#include <RTClib.h> // Librari Real-Time Clock (RTC)
+RTC_DS1307 rtc;     // Inisialisasi objek RTC
 
-#include <LiquidCrystal_I2C.h>  // Librari komunikasi I2C dengan LCD
-LiquidCrystal_I2C lcd(0x27, 16, 2);  // Alamat, kolom, dan baris LCD
+#include <LiquidCrystal_I2C.h>      // Librari komunikasi I2C dengan LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Alamat, kolom, dan baris LCD
 
 // Array untuk menyimpan nama bulan dan hari
 char namaBulan[12][4] = {"Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des"};
 char namaHari[7][7] = {"Ahad", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"};
 
-// Array untuk menyimpan waktu sholat yang dihitung dan perbedaan zona waktu
+// Array untuk menyimpan waktu sholat yang dihitung
 double times[sizeof(TimeName) / sizeof(char *)];
-int GMT = 7;  // Perbedaan zona waktu (WIB 7 / WITA 8 / WIT 9)
+
+// Atur zona waktu, lintang, dan bujur
+int GMT = 7; // Perbedaan zona waktu (WIB 7 / WITA 8 / WIT 9)
+float latitude = -6.1702922;  // Lintang lokasi Anda
+float longitude = 106.831721; // Bujur lokasi Anda
+
+int hours, minutes;
 
 // Fungsi inisialisasi dan mengatur parameter perhitungan waktu sholat
-void setup() {
-  lcd.init();  // Inisialisasi LCD
-  lcd.backlight();  // Nyalakan lampu latar LCD
+void setup()
+{
+  lcd.init();      // Inisialisasi LCD
+  lcd.backlight(); // Nyalakan lampu latar LCD
 
-  rtc.begin();  // Inisialisasi RTC
+  rtc.begin(); // Inisialisasi RTC
 
   // Hapus komentar pada baris di bawah untuk mengatur waktu awal RTC (ganti dengan waktu awal yang Anda inginkan)
   // rtc.adjust(DateTime(2024, 12, 31, 16, 50, 40));
 
   // Atur parameter perhitungan waktu sholat
-  set_calc_method(Makkah);  // Metode perhitungan, opsi ada di PrayerTimes.h
-  set_asr_method(Shafii);  // Metode perhitungan Ashar
-  set_high_lats_adjust_method(AngleBased);  // Metode penyesuaian tinggi lintang
-  set_fajr_angle(20);  // Sudut Fajar (KEMENAG 20 / MESIR 19.5)
-  set_isha_angle(18);  // Sudut Isya' (KEMENAG 18 / MESIR 17.5)
+  set_calc_method(Makkah);                 // Metode perhitungan, opsi ada di PrayerTimes.h
+  set_asr_method(Shafii);                  // Metode perhitungan Ashar
+  set_high_lats_adjust_method(AngleBased); // Metode penyesuaian tinggi lintang
+  set_fajr_angle(20);                      // Sudut Fajar (KEMENAG 20 / MESIR 19.5)
+  set_isha_angle(18);                      // Sudut Isya' (KEMENAG 18 / MESIR 17.5)
 }
 
 // Fungsi untuk menampilkan tanggal Masehi di LCD
-void tampilkanTanggal() {
-  DateTime now = rtc.now();  // Dapatkan waktu saat ini dari RTC
+void tampilkanTanggal()
+{
+  DateTime now = rtc.now(); // Dapatkan waktu saat ini dari RTC
 
-  const char* bulan = namaBulan[now.month() - 1];  // Kurangi 1 karena indeks namaBulan dimulai dari 0, bukan 1
+  const char *bulan = namaBulan[now.month() - 1]; // Kurangi 1 karena indeks namaBulan dimulai dari 0, bukan 1
   uint16_t tgl = now.day();
 
   lcd.setCursor(2, 0);
@@ -44,16 +52,20 @@ void tampilkanTanggal() {
 }
 
 // Fungsi untuk mencetak digit nol di depan jika diperlukan
-void digitNol(int digits) {
-  if (digits < 10) {
+void digitNol(int digits)
+{
+  if (digits < 10)
+  {
     lcd.print('0');
     lcd.print(digits);
   }
-  else lcd.print(digits);
+  else
+    lcd.print(digits);
 }
 
 // Fungsi untuk menampilkan hari di LCD
-void tampilkanHari() {
+void tampilkanHari()
+{
   DateTime now = rtc.now(); // Dapatkan waktu saat ini dari RTC
 
   lcd.setCursor(0, 1);
@@ -61,7 +73,8 @@ void tampilkanHari() {
 }
 
 // Fungsi untuk menampilkan waktu di LCD
-void tampilkanJam() {
+void tampilkanJam()
+{
   DateTime now = rtc.now(); // Dapatkan waktu saat ini dari RTC
 
   lcd.setCursor(8, 1);
@@ -79,7 +92,8 @@ void tampilkanJam() {
 }
 
 // Fungsi untuk menampilkan halaman 1 di LCD
-void displayPage1() {
+void displayPage1()
+{
   unsigned long pageStartTime = millis(); // Dapatkan waktu saat ini
 
   // Tampilkan tanggal Masehi
@@ -89,22 +103,19 @@ void displayPage1() {
   tampilkanHari();
 
   // Tampilkan jam selama 3 detik
-  while (millis() - pageStartTime < 5000) {
+  while (millis() - pageStartTime < 5000)
+  {
     tampilkanJam();
   }
 }
 
 // Fungsi untuk menampilkan halaman 2 di LCD
-void displayPage2() {
+void displayPage2()
+{
   // Dapatkan waktu saat ini dari RTC
   DateTime now = rtc.now();
-
-  // Atur zona waktu, lintang, dan bujur
-  int gmt = 7;
-  float latitude = -6.1702922; // Lintang lokasi Anda
-  float longitude = 106.831721; // Bujur lokasi Anda
+  
   get_prayer_times(now.year(), now.month(), now.day(), latitude, longitude, gmt, times);
-  int hours, minutes;
 
   // Imsak
   get_float_time_parts(times[0], hours, minutes);
@@ -125,16 +136,12 @@ void displayPage2() {
 }
 
 // Fungsi untuk menampilkan halaman 4 di LCD
-void displayPage3() {
+void displayPage3()
+{
   // Dapatkan waktu saat ini dari RTC
   DateTime now = rtc.now();
-
-  // Atur zona waktu, lintang, dan bujur
-  int gmt = 7;
-  float latitude = -6.1702922; // Lintang lokasi Anda
-  float longitude = 106.831721; // Bujur lokasi Anda
+  
   get_prayer_times(now.year(), now.month(), now.day(), latitude, longitude, gmt, times);
-  int hours, minutes;
 
   // Syuruq
   get_float_time_parts(times[1], hours, minutes);
@@ -156,16 +163,12 @@ void displayPage3() {
 }
 
 // Fungsi untuk menampilkan halaman 5 di LCD
-void displayPage4() {
+void displayPage4()
+{
   // Dapatkan waktu saat ini dari RTC
   DateTime now = rtc.now();
-
-  // Atur zona waktu, lintang, dan bujur
-  int gmt = 7;
-  float latitude = -6.1702922; // Lintang lokasi Anda
-  float longitude = 106.831721; // Bujur lokasi Anda
+  
   get_prayer_times(now.year(), now.month(), now.day(), latitude, longitude, gmt, times);
-  int hours, minutes;
 
   // Ashar
   get_float_time_parts(times[3], hours, minutes);
@@ -187,16 +190,12 @@ void displayPage4() {
 }
 
 // Fungsi untuk menampilkan halaman 6 di LCD
-void displayPage5() {
+void displayPage5()
+{
   // Dapatkan waktu saat ini dari RTC
   DateTime now = rtc.now();
-
-  // Atur zona waktu, lintang, dan bujur
-  int gmt = 7;
-  float latitude = -6.1702922; // Lintang lokasi Anda
-  float longitude = 106.831721; // Bujur lokasi Anda
+  
   get_prayer_times(now.year(), now.month(), now.day(), latitude, longitude, gmt, times);
-  int hours, minutes;
 
   // Isya'
   get_float_time_parts(times[6], hours, minutes);
@@ -209,9 +208,10 @@ void displayPage5() {
 }
 
 // Fungsi untuk menampilkan semua halaman secara berurutan
-void displayAllPages() {
+void displayAllPages()
+{
   displayPage1();
-  //delay(3000); // delay di sini dimatikan karena digantikan delay di millis displayPage1
+  // delay(3000); // delay di sini dimatikan karena digantikan delay di millis displayPage1
   lcd.clear();
   displayPage2();
   delay(3000);
@@ -226,7 +226,8 @@ void displayAllPages() {
 }
 
 // Fungsi loop utama
-void loop() {
+void loop()
+{
   lcd.clear();
   displayAllPages();
   delay(2000);
